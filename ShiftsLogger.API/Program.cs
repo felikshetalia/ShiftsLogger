523483fa3fb42ef8232a9035e36b4bb4be03fdb8
felikshetalia <mccorsica@yahoo.com>
2026-09-15 15:25:02 +0300

    deliverable 1 : initialize solution + opening swagger ui

--- ShiftsLogger.API/Program.cs
+++ ShiftsLogger.API/Program.cs
@@ -1,7 +1,11 @@
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
@@ -11,7 +15,8 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
